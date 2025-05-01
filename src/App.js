0e8105df96234cf81db968713ec16dc8d1422054
Divyanshi Gupta <71611825+realdivs@users.noreply.github.com>
2025-05-01 15:07:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import { ToastContainer } from "./components/ToastContainer";
+import "./styles.css";
+
+export default function App() {
+  return <ToastContainer />;
+}
---
+++ src/components/ToastContainer.jsx
@@ -0,0 +1,48 @@
+import { useState, useRef, useEffect } from "react";
+export const ToastContainer = () => {
+  const [toasts, setToasts] = useState([]);
+  const timersRef = useRef({});
+
+  const handleClose = (id) => {
+    clearTimeout(timersRef.current[id]);
+    delete timersRef.current[id];
+
+    setToasts((prevToasts) => {
+      const filteredArr = prevToasts.filter((toast) => {
+        return toast.id !== id;
+      });
+      return filteredArr;
+    });
+  };
+
+  const handleAdd = (message, type) => {
+    const id = new Date().getTime();
+    const newToasts = [...toasts, { id, message, type }];
+    setToasts(newToasts);
+    timersRef.current[id] = setTimeout(() => handleClose(id), 3000);
+  };
+
+  return (
+    <div className="container">
+      <div className="toast-container">
+        {toasts.map(({ id, message, type }) => (
+          <div className={`toast ${type}`} key={id}>
+            {message} <span onClick={() => handleClose(id)}>✖</span>
+          </div>
+        ))}
+      </div>
+
+      <div className="btn-container">
+        <button onClick={() => handleAdd("Success", "success")}>
+          Success Toast
+        </button>
+        <button onClick={() => handleAdd("Info", "info")}>Info Toast</button>
+        <button onClick={() => handleAdd("Warning", "warning")}>
+          {" "}
+          Warning Toast
+        </button>
+        <button onClick={() => handleAdd("Error", "error")}>Error Toast</button>
+      </div>
+    </div>
+  );
+};
